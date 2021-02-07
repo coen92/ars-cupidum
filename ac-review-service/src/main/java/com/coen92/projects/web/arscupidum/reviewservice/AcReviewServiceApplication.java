baee759f90e48644c5a5206e79624e395a3a223e
coen92 <piotrkapica92@gmail.com>
2021-02-07 21:17:57 +0100

    AC: Review service basic main setup

---
+++ ac-review-service/src/main/java/com/coen92/projects/web/arscupidum/reviewservice/AcReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coen92.projects.web.arscupidum.reviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcReviewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcReviewServiceApplication.class, args);
+    }
+
+}
---
+++ ac-review-service/src/test/java/com/coen92/projects/web/arscupidum/reviewservice/AcReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coen92.projects.web.arscupidum.reviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcReviewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
